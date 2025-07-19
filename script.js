d698c81f362323fff36e04fc985a3e647aa91029
Nadia-sg <nad.programa@gmail.com>
2025-07-18 21:28:27 -0300

    versión 2. Botones visibles

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const video = document.querySelector(".custom-video__video");
   const control = document.querySelector(".custom-video__control");
+  const btnUbicacion = document.getElementById("btnUbicacion");
+  const btnConfirmar = document.getElementById("btnConfirmar");
 
   control.addEventListener("click", () => {
     if (video.paused) {
       video.play();
       control.textContent = "||";
-
-      // Oculta el botón después de 3 segundos
       setTimeout(() => {
         control.style.display = "none";
       }, 3000);
@@ -23,7 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
     control.style.display = "flex";
   });
 
-  // Si querés que tocando el video también pause/reproduzca:
   video.addEventListener("click", () => {
     if (!video.paused) {
       video.pause();
@@ -31,4 +30,21 @@ document.addEventListener("DOMContentLoaded", () => {
       control.style.display = "flex";
     }
   });
+
+  // MOSTRAR BOTONES A LOS 2 SEGUNDOS (para testear)
+  video.addEventListener("timeupdate", () => {
+    if (video.currentTime >= 23){
+      btnUbicacion.classList.add("show");
+      btnConfirmar.classList.add("show");
+    }
+  });
+
+  // OCULTAR SI EL VIDEO VUELVE AL INICIO
+  video.addEventListener("seeked", () => {
+    if (video.currentTime < 23){
+      btnUbicacion.classList.remove("show");
+      btnConfirmar.classList.remove("show");
+    }
+  });
 });
+
